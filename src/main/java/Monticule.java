29207fe8a0ba683cf98c271f8b1c9dae123f8f24
AdBergman <bergman.adrian@gmail.com>
2017-09-28 14:27:46 +0200

    first commit

---
+++ src/main/java/Monticule.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Project Monticule
+ * by Group 10 DSV HT2017
+ * Adrian Bergman, Lovisa Lindström, Christian Rusch
+ */
+
+
+
+public class Monticule {
+
+    //TODO: everything :)
+
+}
