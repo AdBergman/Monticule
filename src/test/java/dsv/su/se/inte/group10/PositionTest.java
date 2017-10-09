b493623d086f7127cbde0d32d3666bf98b49c082
Christian Rusch <info@christianrusch.com>
2017-10-09 18:09:17 +0200

    fixed public

--- src/test/java/dsv/su/se/inte/group10/PositionTest.java
+++ src/test/java/dsv/su/se/inte/group10/PositionTest.java
@@ -3,9 +3,9 @@ package dsv.su.se.inte.group10;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class PositionTest {
+public class PositionTest {
 	
-	Position p = new Position();
+	Position p = new Position(10, 10);
 	
 	@Test
 	public void testPositionExists() {
