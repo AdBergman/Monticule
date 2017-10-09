ae7092af68589be48cea558be9dc9bb5c14022e6
Christian Rusch <info@christianrusch.com>
2017-10-09 14:49:21 +0200

    Added constructors to accept width input when creating object instance.

--- src/main/java/dsv/su/se/inte/group10/MapScreen.java
+++ src/main/java/dsv/su/se/inte/group10/MapScreen.java
@@ -2,7 +2,15 @@ package dsv.su.se.inte.group10;
 
 public class MapScreen {
 	
-	private int width = 600; // standard width for map screen
+	private int width;
+	
+	public MapScreen() {
+		this(600); // default size for map screen.
+	}
+	
+	public MapScreen(int width) {
+		this.width = width;
+	}
 	
 	public int getWidth() {
 		return width;
