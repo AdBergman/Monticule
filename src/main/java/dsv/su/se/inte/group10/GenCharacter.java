10a46f1e6b7ffc1e4d41c3ba9b24dcc007512bd6
AdBergman <bergman.adrian@gmail.com>
2017-10-09 15:33:31 +0200

    GenCharacterTest intial basic tests are written starting on class

---
+++ src/main/java/dsv/su/se/inte/group10/GenCharacter.java
@@ -0,0 +1,9 @@
+package dsv.su.se.inte.group10;
+
+/**
+ * General character will be an abstract class for all characters in the game.
+ */
+
+public class GenCharacter {
+
+}
---
+++ src/test/java/dsv/su/se/inte/group10/GenCharacterTest.java
@@ -0,0 +1,38 @@
+package dsv.su.se.inte.group10;
+
+/**
+ * General character will be an abstract class for all characters in the game.
+ * This is a temporary template test class that should be applied to future sub-classes to GenChar.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GenCharacterTest {
+    GenCharacter genChar = new GenCharacter();
+
+    @Test
+    public void testGenCharacterCreation() {
+        assertNotNull("GenCharacter could not be created", genChar);
+    }
+
+    @Test
+    public void testGenCharacterGetDamage(){
+        assertEquals(1, genChar.getDamage());
+    }
+
+    @Test
+    public void testGenCharacterGetSpeed(){
+        assertEquals(1, genChar.getSpeed());
+    }
+
+    @Test
+    public void testGenCharacterGetMaxHP(){
+        assertEquals(1, genChar.getMaxHP());
+    }
+
+    @Test
+    public void testGenCharacterGetCurrentHP(){
+        assertEquals(1, genChar.getCurrentHP());
+    }
+}
