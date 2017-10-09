69244bdcdc82ff6160631bbe318ecf49585f51b6
AdBergman <bergman.adrian@gmail.com>
2017-10-09 14:35:34 +0200

    structure

--- src/main/java/dsv/su/se/inte/group10/Monticule.java
+++ src/main/java/dsv/su/se/inte/group10/Monticule.java
@@ -1,8 +1,12 @@
+package dsv.su.se.inte.group10;
+
 /**
  * Project Monticule
  * by Group 10 DSV HT2017
  * Adrian Bergman, Lovisa Lindström, Christian Rusch
  */
+
+
 
 
 public class Monticule {
--- src/test/java/dsv/su/se/inte/group10/MonticuleTest.java
+++ src/test/java/dsv/su/se/inte/group10/MonticuleTest.java
@@ -1,3 +1,5 @@
+package dsv.su.se.inte.group10;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
