078ffbd46177df7d95504bf50672d9c5eb5bbda7
Christian Rusch <info@christianrusch.com>
2017-10-09 18:01:19 +0200

    Added test to check that position exists.

---
+++ src/test/java/dsv/su/se/inte/group10/PositionTest.java
@@ -0,0 +1,15 @@
+package dsv.su.se.inte.group10;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class PositionTest {
+	
+	Position p = new Position();
+	
+	@Test
+	public void testPositionExists() {
+		assertNotNull(p);
+	}
+
+}
