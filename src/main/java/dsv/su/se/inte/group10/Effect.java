970322b65e11fab36bfff4809a132a248ad53ecd
Lovisa Lindström <lovisa_lindstrom@hotmail.com>
2017-10-12 15:56:32 +0200

    Effect test class added.

--- src/main/java/dsv/su/se/inte/group10/Effect.java
+++ src/main/java/dsv/su/se/inte/group10/Effect.java
@@ -3,10 +3,21 @@ package dsv.su.se.inte.group10;
 public class Effect {
 	
 	private EffectType type; // kan vara maxHP, currentHP, speed, damage
-	private int effect; // måste vara >0
+	private int value; // måste vara >0
 	
-	public Effect(EffectType type, int effect) {
+	public Effect(EffectType type, int value) {
 		this.type = type;
-		this.effect = effect;
+		if(value <= 0) {
+			throw new IllegalArgumentException("Too low");
+		}
+		this.value = value;
+	}
+	
+	public EffectType getEffectType() {
+		return type;
+	}
+	
+	public int getValue() {
+		return value;
 	}
 }
