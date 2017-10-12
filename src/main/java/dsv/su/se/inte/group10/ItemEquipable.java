26f3aab31ab0f2b2f7dc0e61aa0b6cdeee51bd90
AdBergman <bergman.adrian@gmail.com>
2017-10-12 22:49:49 +0200

    updated ItemEquipable to use ArrayList<Effect> as data structure. Updated tests and domain model to match.

--- src/main/java/dsv/su/se/inte/group10/ItemEquipable.java
+++ src/main/java/dsv/su/se/inte/group10/ItemEquipable.java
@@ -1,45 +1,36 @@
 package dsv.su.se.inte.group10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ItemEquipable extends Item{
 
 	protected enum EquipmentType{
 		MAINHAND, OFFHAND, HELMET, CHESTPLATE, LEGGINGS, BOOTS;
 	}
 
-	private int maxHP;
-	private int speed;
-	private int damage;
 	private EquipmentType et;
+	private ArrayList<Effect> effectList;
 
-	public ItemEquipable(String name, int maxHP, int speed, int damage, EquipmentType et) {
+	public ItemEquipable(String name, EquipmentType et, Effect... effects) {
 		super(name);
-		this.maxHP = maxHP;
-		this.speed = speed;
-		this.damage = damage;
 		this.et = et;
+		effectList = new ArrayList<Effect>();
+		effectList.addAll(Arrays.asList(effects));
 	}
 
-	public ItemEquipable(String name, int maxHP, int speed, int damage, EquipmentType et, int weight, int value) {
+	public ItemEquipable(String name, EquipmentType et, int weight, int value, Effect... effects) {
 		super(name, weight, value);
-		this.maxHP = maxHP;
-		this.speed = speed;
-		this.damage = damage;
 		this.et = et;
-	}
-
-	public int getMaxHP() {
-		return this.maxHP;
-	}
-
-	public int getSpeed() {
-		return this.speed;
-	}
-
-	public int getDamage() {
-		return this.damage;
+		effectList = new ArrayList<Effect>();
+		effectList.addAll(Arrays.asList(effects));
 	}
 
 	public EquipmentType getEquipmentType() {
 		return this.et;
+	}
+
+	public ArrayList<Effect> getEffects(){
+		return new ArrayList<Effect>(effectList);
 	}
 }
--- src/test/java/dsv/su/se/inte/group10/ItemEquipableTest.java
+++ src/test/java/dsv/su/se/inte/group10/ItemEquipableTest.java
@@ -7,37 +7,73 @@ import dsv.su.se.inte.group10.ItemEquipable.EquipmentType;
 import static org.junit.Assert.*;
 public class ItemEquipableTest {
 
-	ItemEquipable ie = new ItemEquipable("Sword", 1, 2, 3, EquipmentType.MAINHAND );
+	Effect dmg = new Effect(StatType.damage, 999);
+	Effect maxHP = new Effect(StatType.maxHP, 99);
+	Effect speed = new Effect(StatType.speed, 9);
+	ItemEquipable item = new ItemEquipable("Cheat!", EquipmentType.MAINHAND, dmg, maxHP, speed);
 
 	@Test
 	public void testCreateItemEquipable() {
-		assertNotNull(ie);
+		assertNotNull(item);
 	}
 
 	@Test
 	public void testCreateItemEquipableWeightValue() {
-		ItemEquipable ie2 = new ItemEquipable("Sword", 1, 2, 3, EquipmentType.MAINHAND, 4, 5);
-		assertNotNull(ie2);
-	}
-
-	@Test
-	public void testAttributeMaxHP() {
-		assertEquals(1, ie.getMaxHP());
-	}
-
-	@Test
-	public void testAttributeSpeed() {
-		assertEquals(2, ie.getSpeed());
+		ItemEquipable item2 = new ItemEquipable("Pillow", EquipmentType.OFFHAND, 1, 1);
+		assertNotNull(item2);
 	}
 
 	@Test
 	public void testAttributeDamage() {
-		assertEquals(3, ie.getDamage());
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.damage))
+				assertEquals(dmg, e);
+		}
+	}
+
+	@Test
+	public void testAttributeDamageValue() {
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.damage))
+				assertEquals(dmg.getValue(), e.getValue());
+		}
+	}
+
+	@Test
+	public void testAttributeMaxHP() {
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.maxHP))
+				assertEquals(maxHP, e);
+		}
+	}
+
+	@Test
+	public void testAttributeMaxHPValue() {
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.maxHP))
+				assertEquals(maxHP.getValue(), e.getValue());
+		}
+	}
+
+	@Test
+	public void testAttributeSpeed() {
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.speed))
+				assertEquals(speed, e);
+		}
+	}
+
+	@Test
+	public void testAttributeSpeedValue() {
+		for (Effect e: item.getEffects()) {
+			if (e.getEffectType().equals(StatType.speed))
+				assertEquals(speed.getValue(), e.getValue());
+		}
 	}
 
 	@Test
 	public void testEquipmentType() {
-		assertEquals(EquipmentType.MAINHAND, ie.getEquipmentType());
+		assertEquals(EquipmentType.MAINHAND, item.getEquipmentType());
 
 	}
 
