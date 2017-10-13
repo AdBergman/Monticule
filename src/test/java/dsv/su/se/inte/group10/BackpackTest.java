85c382da3ffb80c060dd6120be5aee30e39134c4
SebastianAg <painfulpower96@gmail.com>
2017-10-13 13:01:04 +0200

    earlyBackpack

---
+++ src/test/java/dsv/su/se/inte/group10/BackpackTest.java
@@ -0,0 +1,25 @@
+package dsv.su.se.inte.group10;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BackpackTest {
+	
+	Backpack bp = new Backpack(10,20);
+	
+	@Test
+	public void testIsBackpack() {
+		assertNotNull(bp);
+	}
+	
+	
+	@Test
+	public void testBackpackLimit() {
+		assertEquals(10, bp.getItemLimit());
+	}
+	
+	@Test
+	public void testBackpackWeight() {
+		assertEquals(20, bp.getWeightLimit());
+	}
+}
