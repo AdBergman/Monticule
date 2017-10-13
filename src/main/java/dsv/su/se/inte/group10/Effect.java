33f97abb3a397b036f11420e52ea6141c1d58af4
Lovisa Lindström <lovisa_lindstrom@hotmail.com>
2017-10-13 11:11:34 +0200

    commit effect.java

--- src/main/java/dsv/su/se/inte/group10/Effect.java
+++ src/main/java/dsv/su/se/inte/group10/Effect.java
@@ -3,7 +3,7 @@ package dsv.su.se.inte.group10;
 public class Effect {
 	
 	private StatType type; // kan vara maxHP, currentHP, speed, damage
-	private int value; // måste vara >0
+	private int value; // måste vara > 0
 	
 	public Effect(StatType type, int value) {
 		if(value <= 0) {
